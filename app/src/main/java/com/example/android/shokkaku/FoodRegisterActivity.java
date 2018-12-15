ea7ab87555cf547c5bc971c74387840c5e9f4179
koiKing <progress125@outlook.jp>
2018-12-15 13:03:53 +0900

    spinner の動的追加機能

--- app/src/main/java/com/example/android/shokkaku/FoodRegisterActivity.java
+++ app/src/main/java/com/example/android/shokkaku/FoodRegisterActivity.java
@@ -1,7 +1,22 @@
 package com.example.android.shokkaku;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodRegisterActivity extends AppCompatActivity {
 
@@ -9,5 +24,76 @@ public class FoodRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_register);
+        //ActionBarを取得
+        ActionBar actionBar = getSupportActionBar();
+        //ActionBarの「戻る」メニューをオプションメニューに追加
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //spinnerの要素を生成
+        setSpinner(FoodRegisterActivity.this);
+
     }
+
+    /**
+     * spinnerに値をセット
+     */
+    private void setSpinner(Context context){
+        Spinner spShop = (Spinner) findViewById(R.id.spShopName);
+        List<String> shopNames = getShopName(context);
+        Log.d("setSpinner", "shopNames:" + shopNames.size());
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(FoodRegisterActivity.this,
+                android.R.layout.simple_spinner_item,shopNames);
+        Log.d("FoodRegisterActivity", "" + FoodRegisterActivity.this);
+        //選択肢のリストが表示されたときに使用するレイアウトを指定する
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //アダプターをスピナーに適応
+        spShop.setAdapter(arrayAdapter);
+
+    }
+/**
+ * spinnerの要素をDBから店名を取得
+ */
+    public List<String> getShopName(Context context) {
+        List<String> shopNameList = new ArrayList<>();
+        //データベースヘルパーオブジェクトを作成
+        DatabaseHelper helper = new DatabaseHelper(context);
+        //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
+        //読み込み専用
+        SQLiteDatabase db = helper.getReadableDatabase();
+        try{
+            String sqlGetShopName = "SELECT shopName FROM shopName_t;";
+
+            //SQL文実行
+            Cursor cursor = db.rawQuery(sqlGetShopName,null);
+            Log.d("FoodRegisterActivity","取得できた行の数" + cursor.getCount());
+            //データがなかった時の初期値を用意
+            String shopName = "";
+            //SQL実行の戻り値であるカーソルオブジェクトをループさせてデータベース内のデータを取得
+            while (cursor.moveToNext()) {
+                shopName = cursor.getString(cursor.getColumnIndex("shopName"));
+                Log.d("getShopName", "shopName: " + shopName);
+                shopNameList.add(shopName);
+            }
+            Log.d("getShopName","格納数 " + shopNameList.size());
+            return shopNameList;
+        }finally {
+            db.close();
+        }
+    }
+
+    /**
+     * オプションメニュー選択時の処理
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //選択されたメニューidの取得
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case android.R.id.home://R値はSDKで用意されているので固定
+                //戻るメニューを押されたときの処理
+                finish();
+        }
+        //親クラスの同名メソッドを呼び出し。その戻り値を返却
+        return super.onOptionsItemSelected(item);
+    }
+
 }
--- app/src/main/java/com/example/android/shokkaku/SeachVegListFragment.java
+++ app/src/main/java/com/example/android/shokkaku/SeachVegListFragment.java
@@ -14,11 +14,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -35,9 +37,10 @@ public class SeachVegListFragment extends Fragment  {
     private ListView _listView;
     List< Map<String,Object>> _vegList;
     //private List< Map<String,Object>> vegiInfo;
+    Toast _toast;
      private static final String[] FROM = {"name","price","shopName"};
      private static  final int[] TO = {R.id.tvFoodName,R.id.tvFoodPrice,R.id.tvShopName};
-
+    Context _context;
     public SeachVegListFragment() {
         // Required empty public constructor
     }
@@ -50,7 +53,7 @@ public class SeachVegListFragment extends Fragment  {
         //nullがかえって来ていたがFragmentのライフサイクルのうちonAttach()からonDetach()まで利用可能
         //ライフサイクルの終了したFragmentがActivityへの参照を持ち続けると、メモリリークに繋がるため
         //非同期処理でgetActivity()を呼び出すべきではない
-          Context _context = getActivity();
+          _context = getActivity();
         Log.d("SeachVegListFragment", "" + _context);
         View view =  inflater.inflate(R.layout.fragment_seach_veg_list, container, false);
 
@@ -60,20 +63,42 @@ public class SeachVegListFragment extends Fragment  {
         SimpleAdapter adapter = new SimpleAdapter(_context,_vegList,R.layout.row,FROM,TO);
         //アダプターの登録
         _listView.setAdapter(adapter);
-        //TODO リストスナクラスの登録TODO
-       // _listView.setOnItemClickListener(new List);
+        Log.d("onCreateView","アダプターの登録");
+        //TODO リスナクラスの登録
+        _listView.setOnItemClickListener(new ListItemClickLitener());
+        Log.d("onCreateView","リスナクラスの登録");
         return view;
-
     }
+
+    /**
+     *リストがタップされたときの処理
+     */
+    private class ListItemClickLitener implements AdapterView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Log.d("_toast","" + _toast);
+            //TODO 連続タップで反応しないように
+                //タップされた行のデータを取得
+                Map<String, Object> vegInfo = (Map<String, Object>) parent.getItemAtPosition(position);
+                Log.d("ListItemClickLitener", "" + vegInfo);
+                //食品の判定方法を取得
+                String judge = (String) vegInfo.get("judge");
+                Log.d("ListItemClickLitener", judge);
+                //トーストの表示
+               Toast.makeText(_context, judge, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 /**
  * DBを参照して野菜の一覧を表示
  */
     public List< Map<String,Object>> initVegList(Context _context,View view){
         Map<String,Object> vegInfo;
         List< Map<String,Object>> vegList = new ArrayList<>();
-//        LocalDate localDate;
-//        //APIレベル使用不可
-//        int month = localDate.getDayOfMonth();
+
+//        APIレベル使用不可
+//        LocalDate localDate; int month = localDate.getDayOfMonth();
         Calendar cal = Calendar.getInstance();
         int month = cal.get(Calendar.MONTH) + 1;
         Log.d("SeachVegListFragment","現在の月" + month);
@@ -84,9 +109,10 @@ public class SeachVegListFragment extends Fragment  {
         //データベースヘルパーオブジェクトを作成
         DatabaseHelper helper = new DatabaseHelper(_context);
         //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
-        SQLiteDatabase db = helper.getWritableDatabase();
+        //読み込み専用
+        SQLiteDatabase db = helper.getReadableDatabase();
         try{
-            //TODO checkboxに対応させる
+            //TODO checkboxに対応させる そもそもcheckboxが必要なのかを検討
             //FULL OUTER JOIN がいいかもしれない
             //情報取得用SQL
            String sqlGetVL = "SELECT MIN(price) as price,shopName,vegetablesName,vegetablesJudge FROM vegetablesInfo_t AS a " +
@@ -99,8 +125,6 @@ public class SeachVegListFragment extends Fragment  {
            Log.d("SeachVegListFragment","SELECT文取得成功");
            //SQL文の実行                            バインド変数
             Cursor cursor = db.rawQuery(sqlGetVL,null);
-            Log.d("SeachVegListFragment","取得できた行の数" + cursor.getCount());
-
             Log.d("SeachVegListFragment","取得できた行の数" + cursor.getCount());
             //データがなかった時の初期値を用意
             int price = 0;
@@ -143,7 +167,6 @@ public class SeachVegListFragment extends Fragment  {
     public void onStart() {
         super.onStart();
     }
-
 
 
 
